23fda9ecc39f03de5ab48a4e897c40ce26c1975e
Jonathan García <jona03g97@gmail.com>
2022-03-11 17:09:16 -0600

    Add: rticket and replacement routes to server

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const cors = require('cors')
 const config = require('./config')
 const alert = require('./routes/alert/route')
 const key = require('./routes/key/route')
+const replacement = require('./routes/replacement/route')
+const ticket = require('./routes/ticket/route')
 const connectDB = require('./db')
 const app = express()
 
@@ -14,6 +16,8 @@ app.set('port', config.port)
 
 app.use('/alert', alert)
 app.use('/key', key)
+app.use('/replacement', replacement)
+app.use('/ticket', ticket)
 
 app.listen(app.get('port'), () => {
   console.log(`Listen on http://localhost:${app.get('port')}`)
