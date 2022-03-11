2484dd41d45b7a1466cc5a2b475a6ae508cb0c08
Jonathan García <jona03g97@gmail.com>
2022-03-11 17:08:49 -0600

    Fix: ticket route

--- src/routes/ticket/route.js
+++ src/routes/ticket/route.js
@@ -2,12 +2,20 @@ const express = require('express')
 const router = express.Router()
 const Model = require('./model')
 
+router.get('/', async (req, res) => {
+  try {
+    const ticket = await Model.find()
+    res.json(ticket)
+  } catch (error) {
+    console.log(error.message)
+    res.send(`No se pudo obtener la alerta`)
+  }
+})
+
 router.get('/:id', async (req, res) => {
   try {
     const ticket = await Model.findById(req.params.id)
-    res.json({
-      ticket
-    })
+    res.json(ticket)
   } catch (error) {
     console.log(error.message)
     res.send(`No se pudo obtener la alerta`)
@@ -15,18 +23,19 @@ router.get('/:id', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  const {
-    caseNumber,
-    client,
-    date,
-    enterHour,
-    firstReplyHour,
-    engineer,
-    description,
-    actions
-  } = req
-
   try {
+    const {
+      body: {
+        caseNumber,
+        client,
+        date,
+        enterHour,
+        firstReplyHour,
+        engineer,
+        description,
+        actions
+      }
+    } = req
     const newTicket = new Model({
       caseNumber,
       client,
@@ -57,9 +66,11 @@ router.delete('/:id', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   try {
+    const { body, params } = req
+
     await Model.findOneAndUpdate(
-      { _id: req.params.id },
-      { _id: req.params.id, ...body }
+      { _id: params.id },
+      { _id: params.id, ...body }
     )
 
     res.send('Registro actualizado correctamente')
